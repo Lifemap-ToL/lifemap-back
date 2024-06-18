56deace5037e549aaea0303f2fc6d20a29aa3124
Julien Barnier <julien.barnier@cnrs.fr>
2024-06-18 15:33:25 +0200

    feat: add prerender_bbox script

---
+++ builder/prerender_bbox.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+BBOX_CONTAINER=lifemap-bbox
+PRERENDER_THREADS=30
+MAX_ZOOM=8
+
+# Remove cached tiles
+echo "- REMOVE OLD TILES"
+docker exec -t $BBOX_CONTAINER rm -rf /opt/data/*
+
+# Compute tiles for the 5 first zoom levels on 7 threads
+echo "- SEEDING TILES"
+docker exec -t $BBOX_CONTAINER sh -c "bbox-server seed --tileset lifemap --minzoom 4 --maxzoom $MAX_ZOOM --threads $PRERENDER_THREADS"
