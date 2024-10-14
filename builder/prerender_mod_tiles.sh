e98249edb27f8d9233e0064ad4ea7e73db52bff4
Julien Barnier <julien.barnier@cnrs.fr>
2024-10-14 13:45:03 +0200

    fix: docker command with * must be quoted and passed to sh

--- builder/prerender_mod_tiles.sh
+++ builder/prerender_mod_tiles.sh
@@ -9,11 +9,8 @@ docker exec -t $MOD_TILE_CONTAINER killall render_list
 
 # Remove ALL old tiles
 echo "- REMOVE OLD TILES"
-docker exec -t $MOD_TILE_CONTAINER rm -r /var/lib/mod_tile/*
+docker exec -t $MOD_TILE_CONTAINER sh -c "rm -r /var/lib/mod_tile/*"
 
-# Restart services
-#echo "- RESTART SERVICES"
-#docker restart $MOD_TILE_CONTAINER
 
 # Compute tiles for the 5 first zoom levels on 7 threads
 echo "- PRERENDER TILES"
